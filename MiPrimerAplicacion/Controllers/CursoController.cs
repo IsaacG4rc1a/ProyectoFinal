708c166bedadf889f1da39d2bfa73e2b79b9f87d
IsaacG4rc1a <isaac.g.17.98@gmail.com>
2020-01-09 11:03:40 -0600

    Add project files.

---
+++ MiPrimerAplicacion/Controllers/CursoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiPrimerAplicacion.Controllers
+{
+    public class CursoController : Controller
+    {
+        // GET: Curso
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Mensaje()
+        {
+            return "Bienvenidos al curos de MVC 5";
+        }
+
+        public string Saludo(string nombre)
+        {
+            return "Hola, " + nombre + ". ¿Cómo estás?";
+        }
+    }
+}
